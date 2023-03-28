fc6c43514dee67ca72a635d3867d87efa33d105d
G0l1at <diego.jauregui.salvatierra@gmail.com>
2023-03-28 11:26:47 -0400

    refactor: add new packages and improve configurations

--- gradle/src/main/java/com/sample/gradle/config/WebSecurityConfigurer.java
+++ gradle/src/main/java/com/sample/gradle/config/WebSecurityConfigurer.java
@@ -1,21 +1,25 @@
 package com.sample.gradle.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-    http
-      .authorizeRequests()
-      .anyRequest()
-      .permitAll();
+public class WebSecurityConfigurer {
 
-    http.csrf().disable();
-    
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests()
+        .anyRequest()
+        .permitAll();
+
+    http
+        .csrf(csrf -> csrf.disable());
+
     return http.build();
   }
 }
