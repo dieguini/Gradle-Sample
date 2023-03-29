99994291f193d82aebabf995611d179b57820b5e
G0l1at <diego.jauregui.salvatierra@gmail.com>
2023-03-29 10:31:53 -0400

    refactor: minor change (library not used)

--- gradle/src/main/java/com/sample/gradle/controllers/DevOpsController.java
+++ gradle/src/main/java/com/sample/gradle/controllers/DevOpsController.java
@@ -1,11 +1,10 @@
-package com.sample.gradle.controller;
+package com.sample.gradle.controllers;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
