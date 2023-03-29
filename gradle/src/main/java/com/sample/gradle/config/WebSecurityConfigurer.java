30f99b21c091de84313555d53136ff1b01a8387c
G0l1at <diego.jauregui.salvatierra@gmail.com>
2023-03-29 11:14:42 -0400

    feat: minor stylish

--- gradle/src/main/java/com/sample/gradle/config/WebSecurityConfigurer.java
+++ gradle/src/main/java/com/sample/gradle/config/WebSecurityConfigurer.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -49,9 +48,9 @@ public class WebSecurityConfigurer {
     // Set permissions on endpoints
     http
         .authorizeHttpRequests(authorize -> authorize
-            // Our public endpoints
+            // Public endpoints
             .antMatchers("/**/auth/**").permitAll()
-            // Our private endpoints
+            // Private endpoints
             .anyRequest().authenticated());
 
     // Add JWT token filter
